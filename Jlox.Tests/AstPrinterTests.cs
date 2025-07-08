3f6fcc5129f9c8da17be36061bca4366f50d5a6e
Arthur Cousseau <arthur.cousseau@me.com>
2025-07-08 14:42:56 +0200

    Add AstPrinter + test lib (end of chapter 5)

---
+++ Jlox.Tests/AstPrinterTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Jlox.Tests;
+
+public sealed class AstPrinterTests
+{
+    [Fact]
+    public void Correct_output()
+    {
+        Expr expression = new Expr.Binary(
+            new Expr.Unary(
+                new Token(TokenType.Minus, "-", null, 1),
+                new Expr.Literal(123)),
+            new Token(TokenType.Star, "*", null, 1),
+            new Expr.Grouping(
+                new Expr.Literal(45.67f)));
+
+        Assert.Equal("(* (- 123) (group 45.67))", new AstPrinter().Print(expression));
+    }
+}
---
+++ Jlox/AstPrinter.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace Jlox;
+
+public class AstPrinter : Expr.IVisitor<string>
+{
+    #region IVisitor
+    public string VisitBinaryExpr(Expr.Binary expr)
+    {
+        return Parenthesize(expr.Op.Lexeme, expr.Left, expr.Right);
+    }
+
+    public string VisitGroupingExpr(Expr.Grouping expr)
+    {
+        return Parenthesize("group", expr.Expression);
+    }
+
+    public string VisitLiteralExpr(Expr.Literal expr)
+    {
+        if (expr.Value is null) return "nil";
+
+        // This is an addition to the Crafting Interpreters book (chapter 5)
+        if (expr.Value is IFormattable formattable)
+        {
+            return formattable.ToString(null, CultureInfo.InvariantCulture);
+        }
+
+        return expr.Value.ToString() ?? "<null>";
+    }
+
+    public string VisitUnaryExpr(Expr.Unary expr)
+    {
+        return Parenthesize(expr.Op.Lexeme, expr.Right);
+    }
+    #endregion
+
+    public string Print(Expr expr)
+    {
+        return expr.Accept(this);
+    }
+
+    private string Parenthesize(string name, params Expr[] exprs)
+    {
+        StringBuilder builder = new();
+
+        builder.Append('(').Append(name);
+        foreach (Expr expr in exprs)
+        {
+            builder.Append(' ');
+            builder.Append(expr.Accept(this));
+        }
+        builder.Append(')');
+
+        return builder.ToString();
+    }
+}
